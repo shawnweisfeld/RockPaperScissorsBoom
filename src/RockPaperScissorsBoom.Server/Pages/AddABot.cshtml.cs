dca81ec7cccaaf93552c51924c2cd2c10150912f
michaelsrichter <mrichter@microsoft.com>
2018-09-24 17:36:41 -0400

    Cleaned up homepage, added AddABot page

---
+++ src/RockPaperScissorsBoom.Server/Pages/AddABot.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RockPaperScissorsBoom.Server.Pages
+{
+    [Authorize]
+    public class AddABot : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
